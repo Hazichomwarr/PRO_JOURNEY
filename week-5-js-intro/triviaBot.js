1843a8fcbbe756ec9d988415fdf0349269f3690c
hazichomwarr <batchiy6@gmail.com>
2025-05-26 22:02:26 -0400

    week-5 intro to js

---
+++ week-5-js-intro/triviaBot.js
@@ -0,0 +1,15 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Abdiel";
+let botLocation = "New york";
+let favoriteLanguage = "Python" ;
+console.log('My name is '+ botName + " and I live on "+ botLocation + ".");
+
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+let codingFact = `${favoriteLanguage} is fun to learn.`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} is easy to learn`;
+console.log(codingFact);
+codingFact = `${favoriteLanguage} is a popular language`;
+console.log(codingFact);
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`)
