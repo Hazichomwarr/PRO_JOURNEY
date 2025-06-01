8f5d470606c5e9759b68312b689c9cda404a9a8c
hazichomwarr <batchiy6@gmail.com>
2025-06-01 16:32:11 -0400

    moved script to its own file

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const imagesArr = [
-  {
-    src: "images/pic-with-son.jpeg",
-    alt: "Hamza and son in traditionnal attires ",
-  },
-  { src: "images/me&kids.jpg", alt: "Hamza at play with kids" },
-  { src: "images/pic.jpeg", alt: "Hamza in a tux" },
-];
+const hamburger = document.getElementById("hamburger");
+      const navLinks = document.getElementById("nav-links");
 
-let randomIndex = Math.floor(Math.random() * imagesArr.length);
-let randomImg = imagesArr[randomIndex];
+      hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+      });
 
-const headerImg = document.getElementById("hero-imag");
-headerImg.setAttribute("src", `/${randomImg.src}`);
-headerImg.setAttribute("alt", `/${random.alt}`);
+        const imagesArr = [
+          {
+            src: "images/pic-with-son.jpeg",
+            alt: "Hamza and son in traditionnal attires ",
+          },
+          { src: "images/me&kids.jpg", alt: "Hamza at play with kids" },
+          { src: "images/pic.jpeg", alt: "Hamza in a tux" },
+        ];
+
+        let randomIndex = Math.floor(Math.random() * imagesArr.length);
+        let randomImg = imagesArr[randomIndex];
+
+        const headerImg = document.getElementById("hero-imag");
+        headerImg.setAttribute("src", `./${randomImg.src}`);
+        headerImg.setAttribute("alt", `./${randomImg.alt}`);
