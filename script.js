bdc1e7a482fef6373d71ab293fb1d61e1c0f2297
hazichomwarr <batchiy6@gmail.com>
2025-05-31 22:00:08 -0400

    updated script code

---
+++ script.js
@@ -0,0 +1,15 @@
+const imagesArr = [
+  {
+    src: "images/pic-with-son.jpeg",
+    alt: "Hamza and son in traditionnal attires ",
+  },
+  { src: "images/me&kids.jpg", alt: "Hamza at play with kids" },
+  { src: "images/pic.jpeg", alt: "Hamza in a tux" },
+];
+
+let randomIndex = Math.floor(Math.random() * imagesArr.length);
+let randomImg = imagesArr[randomIndex];
+
+const headerImg = document.getElementById("hero-imag");
+headerImg.setAttribute("src", `/${randomImg.src}`);
+headerImg.setAttribute("alt", `/${random.alt}`);
