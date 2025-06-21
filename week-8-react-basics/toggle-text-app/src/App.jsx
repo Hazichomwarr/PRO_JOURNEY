459bc506655beef1958afc91fd8e3bd99078bab7
hazichomwarr <batchiy6@gmail.com>
2025-06-21 15:14:19 -0400

    fruit-search-app

--- week-8-react-basics/toggle-text-app/src/App.jsx
+++ week-8-react-basics/toggle-text-app/src/App.jsx
@@ -3,15 +3,26 @@ import { useState } from 'react'
 function App() {
 
   const [isVisible, setIsVisible] = useState(false);
+  const [isLightMode, setIsLightMode] = useState(false);
 
   const handleToggleVisibility = () => {
     setIsVisible(!isVisible);
   }
 
+  const handleModeTheme = () => {
+    setIsLightMode(!isLightMode)
+  }
   return (
-    <div id="toggle-container">
-      <button onClick={handleToggleVisibility} id="toggle-button">{isVisible ? "Hide Message" : "Show Message"}</button>
+    <div id="toggle-container" className={`${isLightMode ? 'light-mode' : 'dark-mode'}`}>
+      <button onClick={handleToggleVisibility} className="toggle-button">
+        {isVisible ? "Hide Message" : "Show Message"}
+      </button>
+      
       {isVisible && <p id="message">I love freeCodeCamp!</p>}
+
+      <button onClick={handleModeTheme} className="toggle-button">
+       {isLightMode ? "Switch to Light Mode" : "Switch to Dark Mode" }
+      </button>
     </div>
   );
 };
