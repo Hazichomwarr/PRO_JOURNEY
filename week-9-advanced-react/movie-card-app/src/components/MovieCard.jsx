cb197621b8ced0039f78506d6c158b650c2a6dd9
hazichomwarr <batchiy6@gmail.com>
2025-06-29 20:52:23 -0400

    added searchBar to MovieCard.jsx

--- week-9-advanced-react/movie-card-app/src/components/MovieCard.jsx
+++ week-9-advanced-react/movie-card-app/src/components/MovieCard.jsx
@@ -1,22 +1,20 @@
 export const MovieCard = ({ movie, islike, handleLike }) => {
   return (
-    <>
-      <section className="bg-[gold]-200 rounded shadow-sm p-4 max-w-md mb-[2rem] flex flex-col md:flex-row items-center mb-8">
-        <h2 className="movie-hd mb-[.5rem]">{movie.title}</h2>
-        <figure>
-          <img
-            src={movie.poster}
-            alt={`${movie.title} movie poster`}
-            className="w-[250px]"
-          />
-          <figcaption className="flex justify-between items-center font-bold text-gold">
-            <span>{movie.genre}</span>
-            <button className="" onClick={handleLike}>
-              {!islike ? "Like 👍" : "Dislike 👎"}
-            </button>
-          </figcaption>
-        </figure>
-      </section>
-    </>
+    <section className="flex flex-col md:flex-row items-center gap-4 shadow-md rounded-lg mb-6">
+      <img
+        src={movie.poster}
+        alt={`${movie.title} movie poster`}
+        className="w-[400px] h-auto rounded-[10px]"
+      />
+      <article className="flex items-center gap-[2rem] mb-[2rem]">
+        <h2 className="text-xl font-bold text-gray-800">{movie.title}</h2>
+        <p className="text-sm text-gray-500">{movie.genre}</p>
+        <button onClick={handleLike} 
+        className={`mt-2 text-2xl cursor-pointer hover:scale-110 transition-transform justify-self-stretch rounded-[10px] p-[.5rem] border-none bg-[gray] ${islike ? "text-[yellow] bg-[gray]" : "text-[gold] bg-[gray]"
+}`} > 
+          {!islike ? "Like 👍" : "Dislike 👎"}
+        </button>
+      </article>
+    </section>
   );
 };
--- week-9-advanced-react/movie-card-app/src/components/MovieList.jsx
+++ week-9-advanced-react/movie-card-app/src/components/MovieList.jsx
@@ -1,9 +1,13 @@
 import { movies } from "../data/movie";
 import { MovieCard } from "./MovieCard";
-import { useState } from "react";
+import { useState} from "react";
 
 export const MovieList = () => {
   const [likedMovies, setLikedMovies] = useState({});
+  const [isMovieFound, setIsMovieFound] = useState(false)
+  const [movieFound, setMovieFound] = useState()
+  const[searchTerm, setSearchTerm] = useState('')
+  const [isfilter, setIsfilter] = useState(false)
 
   const handleLike = (id) => {
     setLikedMovies((prev) => ({
@@ -11,14 +15,50 @@ export const MovieList = () => {
       [id]: !prev[id],
     }));
   };
+  const handleSearch = (e) => {
+    setIsfilter(true)
+    setSearchTerm(e.target.value.trim())
+    let result = movies.find(movie => movie.title.toLowerCase().includes(e.target.value.trim().toLowerCase()))
+    if (Object.keys(result).length !== 0) { 
+      setIsMovieFound(true)
+      setMovieFound(result)
+    }
+    else {
+    setIsMovieFound(false)
+    setMovieFound({})
+    }
+  
+    return
+  }
 
+  const handleResetBtn = () => {
+    setSearchTerm('')
+    setMovieFound({})
+    setIsfilter(false)
+    setIsMovieFound(false)
+  }
   return (
-    <main className="">
+    <main className="max-w-4xl mx-auto px-[4px] py-[8px] bg-[gold]">
       <h1 className="text-3xl font-bold text-blue-500 text-center mb-[2rem] bg-gray-300">
-        Our Movies
+        🎬 Our Movies
       </h1>
+      <div className="flex justify-center items-center">
+        <input type="search" placeholder="Search movie..." className="my-[2rem] mx-[100px] p-[.5rem] rounded-[10px] w-[50%] text-start text-[1.2rem]" value={searchTerm} onChange={handleSearch}/>
+        {
+          isfilter && <button onClick={handleResetBtn}
+          className="mt-2 text-2xl cursor-pointer hover:scale-110 transition-transform justify-self-stretch rounded-[10px] p-[.6rem] border-none bg-[gray] text-[white]">Reset Search</button>
+        }
+      </div>
       <div className="movies">
-        {movies.map((movie) => (
+          {isMovieFound ? 
+          <MovieCard
+            key={movieFound.id}
+            movie={movieFound}
+            islike={likedMovies[movieFound.id]}
+            handleLike={() => handleLike(movieFound.id)}
+          />
+          : 
+          movies.map((movie) => (
           <MovieCard
             key={movie.id}
             movie={movie}
