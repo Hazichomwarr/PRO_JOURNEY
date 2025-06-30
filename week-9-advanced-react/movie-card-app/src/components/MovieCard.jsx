3f36d12fd2056e09486992f467541baa0c248e4b
hazichomwarr <batchiy6@gmail.com>
2025-06-29 22:09:56 -0400

    updated MovieList.jsx

--- week-9-advanced-react/movie-card-app/src/components/MovieCard.jsx
+++ week-9-advanced-react/movie-card-app/src/components/MovieCard.jsx
@@ -1,12 +1,12 @@
 export const MovieCard = ({ movie, islike, handleLike }) => {
   return (
-    <section className="flex flex-col md:flex-row items-center gap-4 shadow-md rounded-lg mb-6">
+    <section className="grid grid-cols-1 justify-center gap-4 shadow-md rounded-lg mb-6 md:grid-cols-2">
       <img
         src={movie.poster}
         alt={`${movie.title} movie poster`}
         className="w-[400px] h-auto rounded-[10px]"
       />
-      <article className="flex items-center gap-[2rem] mb-[2rem]">
+      <article className="flex items-center gap-[2rem] mb-[2rem] hover:scale-105">
         <h2 className="text-xl font-bold text-gray-800">{movie.title}</h2>
         <p className="text-sm text-gray-500">{movie.genre}</p>
         <button onClick={handleLike} 
--- week-9-advanced-react/movie-card-app/src/components/MovieList.jsx
+++ week-9-advanced-react/movie-card-app/src/components/MovieList.jsx
@@ -4,8 +4,7 @@ import { useState} from "react";
 
 export const MovieList = () => {
   const [likedMovies, setLikedMovies] = useState({});
-  const [isMovieFound, setIsMovieFound] = useState(false)
-  const [movieFound, setMovieFound] = useState()
+  const [moviesFound, setMoviesFound] = useState([])
   const[searchTerm, setSearchTerm] = useState('')
   const [isfilter, setIsfilter] = useState(false)
 
@@ -16,47 +15,38 @@ export const MovieList = () => {
     }));
   };
   const handleSearch = (e) => {
-    setIsfilter(true)
+    setIsfilter(true) // to show the reset-movies button
     setSearchTerm(e.target.value.trim())
-    let result = movies.find(movie => movie.title.toLowerCase().includes(e.target.value.trim().toLowerCase()))
-    if (Object.keys(result).length !== 0) { 
-      setIsMovieFound(true)
-      setMovieFound(result)
-    }
-    else {
-    setIsMovieFound(false)
-    setMovieFound({})
-    }
-  
+    const results = movies.filter(movie => movie.genre.toLowerCase().includes(e.target.value.trim().toLowerCase()))
+    setMoviesFound(results)
     return
   }
 
   const handleResetBtn = () => {
     setSearchTerm('')
-    setMovieFound({})
+    setMoviesFound([])
     setIsfilter(false)
-    setIsMovieFound(false)
+   
   }
   return (
     <main className="max-w-4xl mx-auto px-[4px] py-[8px] bg-[gold]">
       <h1 className="text-3xl font-bold text-blue-500 text-center mb-[2rem] bg-gray-300">
         🎬 Our Movies
       </h1>
-      <div className="flex justify-center items-center">
-        <input type="search" placeholder="Search movie..." className="my-[2rem] mx-[100px] p-[.5rem] rounded-[10px] w-[50%] text-start text-[1.2rem]" value={searchTerm} onChange={handleSearch}/>
-        {
-          isfilter && <button onClick={handleResetBtn}
-          className="mt-2 text-2xl cursor-pointer hover:scale-110 transition-transform justify-self-stretch rounded-[10px] p-[.6rem] border-none bg-[gray] text-[white]">Reset Search</button>
-        }
+      <div className="flex justify-center items-center mb-[2rem]">
+        <input type="search" placeholder="Search movie..." className=" block my-[2rem] mx-[100px] p-[.5rem] rounded-[10px] w-[50%] text-start text-[1.2rem]" value={searchTerm} onChange={handleSearch}/>
+        <button onClick={handleResetBtn}
+          className="font-[1.2rem] hover:scale-110 transition-transform rounded-[10px] px-[2rem] py-[1rem] bg-[gray] text-[white] block active:bg-blue-500 ">All</button>
+        
       </div>
       <div className="movies">
-          {isMovieFound ? 
+          {moviesFound.length !== 0 ? moviesFound.map(movieFound => (
           <MovieCard
             key={movieFound.id}
             movie={movieFound}
             islike={likedMovies[movieFound.id]}
             handleLike={() => handleLike(movieFound.id)}
-          />
+          />))
           : 
           movies.map((movie) => (
           <MovieCard
