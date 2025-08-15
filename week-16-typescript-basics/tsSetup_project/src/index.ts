d89ab105e21e53af428454bf8d01379ff4f17679
hazichomwarr <batchiy6@gmail.com>
2025-08-14 21:14:51 -0400

    added functions in tsSetup_project

--- week-16-typescript-basics/tsSetup_project/src/index.ts
+++ week-16-typescript-basics/tsSetup_project/src/index.ts
@@ -1,12 +1,70 @@
 console.log("Hello!!!!");
-
 const btn = document.getElementById("btn"); // or add '!' <- Non-Null assertion Operator
 
 //assertion with DOM
 const input = document.getElementById("todo")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const ul = document.getElementById("todo-list");
 
-btn?.addEventListener("click", function (): void {
-  console.log("BUTTON CLICKED!");
-  console.log(input.value);
+// btn?.addEventListener("click", function (): void {
+//   console.log("BUTTON CLICKED!");
+//   console.log(input.value);
+//   input.value = "";
+// });
+
+//Connecting to localStorage
+function getTodos(): Todo[] {
+  const todosJSON = localStorage.getItem("todos");
+  if (todosJSON === null) return [];
+  return JSON.parse(todosJSON);
+}
+function saveToLocalStorage() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+//Adding in an interface
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+const todos: Todo[] = getTodos();
+todos.forEach(createNewTodo);
+
+//Working with Events
+function handleSubmit(e: SubmitEvent): void {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  todos.push(newTodo);
+  createNewTodo(newTodo);
   input.value = "";
-});
+  console.log(todos);
+
+  //adding in to localStorage
+  saveToLocalStorage();
+}
+
+function createNewTodo(todo: Todo): void {
+  const newLi = document.createElement("li");
+  const checkbox = document.createElement("input");
+
+  checkbox.type = "checkbox";
+  newLi.append(todo.text);
+  newLi.append(checkbox);
+  ul?.append(newLi);
+
+  checkbox.addEventListener("change", () => {
+    todo.completed = checkbox.checked;
+    if (todo.completed) {
+      newLi.classList.add("completed");
+    } else {
+      newLi.classList.remove("completed");
+    }
+    saveToLocalStorage();
+  });
+}
+
+//document.addEventListener("DOMContentLoaded", () => displayTodos(todos));
+form.addEventListener("submit", handleSubmit);
