b496a6c641affc4136db4b14d98bf8e5de21c527
hazichomwarr <batchiy6@gmail.com>
2025-10-28 10:57:48 -0400

    fix small bug in server.js and db.js

--- week-24-27-coachFinder-app/backend/server.js
+++ week-24-27-coachFinder-app/backend/server.js
@@ -11,16 +11,14 @@ const dashboardRoutes = require("./routes/dashboard");
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-//     credentials: true,
-//   })
-// );
-app.use(cors({ origin: true, credentials: true }));
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
+  })
+);
+// app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
-
 //Connect to db before setting routes
 (async () => {
   const db = await connecDB();
