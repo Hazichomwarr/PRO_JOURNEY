1afd134361e26de5d67f401ca5ea3feb11b9385c
hazichomwarr <batchiy6@gmail.com>
2026-01-20 16:23:11 -0500

    rename pisma project folder

---
+++ week-41-42-prisma-basics/prisma_starter/index.js
@@ -0,0 +1,17 @@
+//index.js
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      email: "test@example.com",
+      name: "Hamza",
+    },
+  });
+
+  console.log("newUser:", user);
+}
+
+main();
