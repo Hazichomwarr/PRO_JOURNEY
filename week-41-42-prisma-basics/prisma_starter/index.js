b87c07aaa5af11db8c0cb14b75b6a3af4db90443
hazichomwarr <batchiy6@gmail.com>
2026-01-20 19:36:47 -0500

    prisma lesson2: one-to-many elationship

--- week-41-42-prisma-basics/prisma_starter/index.js
+++ week-41-42-prisma-basics/prisma_starter/index.js
@@ -4,14 +4,53 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email: "test@example.com",
+  //     name: "Hamza",
+  //   },
+  // });
+
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email: "issa@example.com",
+  //     name: "Issa",
+  //     posts: {
+  //       create: [
+  //         { title: "1st post", content: "Hello prisma world!" },
+  //         { title: "2nd post", content: "I'm Glad to be here!" },
+  //       ],
+  //     },
+  //   },
+  // });
+
   const user = await prisma.user.create({
     data: {
-      email: "test@example.com",
-      name: "Hamza",
+      email: "abdiel@example.com",
+      name: "Abdiel",
+      posts: {
+        create: [
+          {
+            title: "learning JS",
+            content: "Js is really hard to learn",
+            comments: {
+              create: [
+                { text: "I agree with you" },
+                { text: "I find it easy though" },
+              ],
+            },
+          },
+        ],
+      },
     },
   });
+  console.log("new-User:", user);
 
-  console.log("newUser:", user);
+  const users = await prisma.user.findMany({
+    include: { posts: { include: { comments: true } } },
+  });
+  console.log("users-list:", users);
+  console.dir(users, { depth: null });
 }
 
 main();
