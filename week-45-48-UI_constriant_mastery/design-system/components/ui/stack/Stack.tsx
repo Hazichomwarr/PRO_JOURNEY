b348415443b64b168f4668792e9f0d6d08e514c4
hazichomwarr <batchiy6@gmail.com>
2026-03-24 11:57:47 -0400

    update Stack.tsx: add gap-10 and gap-12 tp gapMap object

--- week-45-48-UI_constriant_mastery/design-system/components/ui/stack/Stack.tsx
+++ week-45-48-UI_constriant_mastery/design-system/components/ui/stack/Stack.tsx
@@ -12,6 +12,8 @@ const gapMap: Record<number, string> = {
   4: "gap-4",
   6: "gap-6",
   8: "gap-8",
+  10: "gap-10",
+  12: "gap-12",
 };
 
 export function Stack({ children, gap = 4, className }: StackProps) {
